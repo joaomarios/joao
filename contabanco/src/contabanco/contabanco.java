dfe83a6eec877128387a5e71881376b4edcef33e
user <user@user-PC>
2021-11-19 20:21:02 -0200

    primeiro commit

--- contabanco/src/contabanco/contabanco.java
+++ contabanco/src/contabanco/contabanco.java
@@ -110,32 +110,4 @@ public class contabanco {
     public void setStatus(boolean status) {
         this.status = status;
     }
-
-    public static void main(String[] agrs) {
-
-        contabanco p1 = new contabanco();
-        p1.abrircontar("cp");
-        p1.deposita(500);
-        p1.sacar((float) 49.9);
-        p1.estadoConta();
-
-        contabanco p2 = new contabanco("jose silva", 252, 58545);
-        p2.abrircontar("cc");
-        p2.deposita((float) 150.90);
-        p2.sacar((float) 299.9);
-        p2.estadoConta();
-
-        contabanco p3 = new contabanco();
-        p3.nome = "maria de jesus";
-        p3.agençia = 2525;
-        p3.cpf = "115.589.478.58";
-        p3.numconta = 12539875;
-        p3.status = true;
-        p3.tipo = "cc";
-        p3.saldo = 0;
-        p3.deposita((float) 5000);
-        p3.estadoConta();
-
-    }
-
 }
